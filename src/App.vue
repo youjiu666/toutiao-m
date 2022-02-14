1c30cfc592bf3dbc2575f6518252b22004fc87ad
wangyoujiu <1778535388@qq.com>
2022-02-14 20:22:05 +0800

    项目初始化——引入vent组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
 
 		<router-view />
 		<h1>黑马头条</h1>
+		<div>
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
+			<i class="toutiao toutiao-dianzan2"></i>
+		</div>
+		<van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
 	</div>
 </template>
 <script>
